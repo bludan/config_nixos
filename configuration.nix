77085b2fee70dc3259e07c0bce657a7d95b53e0d
karson <bluzzenith@gmail.com>
2025-07-01 15:32:05 +0200

    Removing further comments + testing permissions

--- configuration.nix
+++ configuration.nix
@@ -10,8 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos"; 
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
