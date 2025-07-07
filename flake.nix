390a9b6c9d7983ad10dc0a01b42e388567c752b7
karson <bluzzenith@gmail.com>
2025-07-07 19:33:26 +0200

    implemented flake structure

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+    	inherit system;
+
+	config = {
+	  allowUnfree = true;
+	  };
+    };
+
+    in 
+    {
+	
+	nixosConfigurations = {
+	 myNixos = nixpkgs.lib.nixosSystem {
+	  specialArgs = {inherit system; };
+
+	  modules = [
+	  ./nixos/configuration.nix
+	  ];
+	};
+       };
+  };
+}
