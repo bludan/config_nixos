9b8f272e32006b94a1e7761cf3cdc3be6ca8e987
karson <bluzzenith@gmail.com>
2025-07-09 16:19:05 +0200

    added nvf for nvim config on nixos

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = { self, nixpkgs, }: 
+  outputs = { self, nixpkgs, nvf, ...}: 
   let 
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -18,7 +19,11 @@
 
     in 
     {
-	
+	packages."84_64-linux".default = 
+	  (nvf.lib.neovimConfiguration {
+	    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+	    modules = [ ./nvf-configuration.nix ];
+	    }).neovim;
 	nixosConfigurations = {
 	 default = nixpkgs.lib.nixosSystem {
 	  specialArgs = {inherit system; };
@@ -26,6 +31,7 @@
 	  modules = [
 	  ./nixos/configuration.nix
 	  ./nixos/dev-tools.nix
+	  nvf.nixosModules.default
 	  ];
 	};
        };
